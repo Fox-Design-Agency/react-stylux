191a204477eedc9c526ceb5e5213604966f090a1
Christopher Fox <cfox1290@gmail.com>
2018-05-10 11:24:52 -0600

    rebuilt button

--- Buttons/buttonFunctions/buttonFunctions.js
+++ Buttons/buttonFunctions/buttonFunctions.js
@@ -17,105 +17,138 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 function buttonFunction(type, props, state, self) {
     var buttonStyles = void 0,
-        cursor = void 0,
-        backgroundColor = void 0,
-        color = "";
+        cursor = "";
+
+    var id = props.id;
+    var className = props.className;
+    var width = props.width;
+    var height = props.height;
+    var fontSize = props.fontSize || '20px';
+    var color = props.color || '#000000';
+    var hoverColor = props.hover || 'white';
+    var hoverBackground = props.hoverBackground || '#000000';
+    var revColor = props.color || '#000000';
+    var frev = props.color || 'white';
+    var background = props.background || '#ffffff';
+    var backgroundColor = props.background || 'white';
+    var blockBackground = props.background || "black";
+    var revBackground = props.background || '#ffffff';
+    var borderRadius = props.borderRadius || '28px';
+    var border = props.border || 'solid #000000 2px';
+    var padding = props.padding || '10px 20px 10px 20px';
+    var margin = props.margin || "0 0 17px 0";
+    var animationIterationCount = props.aniCount;
+    var animationTimingFunction = props.aniTime;
+    var animationName = props.aniName;
+    var animationDuration = props.aniDur;
+    var trandform = props.transform;
+    var trandformrev = props.transform || 'translateY(-4px)';
+    var hovBackground = props.hovBackground || '#eee';
+    var hovBoxShadow = props.hovBoxShadow || '0 5px #666';
+    var floatBoxShadow = props.boxShadow;
+    var boxShadow = props.boxShadow || '0 9px #999';
+    var boxShadowRev = props.boxShadow || '0 9px #999';
+    var transformOrigin = props.transformOrigin;
+    var animationFillMode = props.aniFillMode;
+    var smdis = props.smDis || 'flex';
+    var mddis = props.mdDis || 'flex';
     switch (type) {
         case type = '1':
-            backgroundColor = arguments[4] || state.background;
-            _color = arguments[5] || state.color;
+            backgroundColor = arguments[4] || backgroundColor;
+            color = arguments[5] || color;
             cursor = "pointer";
             return {
                 changeHover: function changeHover() {
-                    var background = self.state.hoverBackground;
-                    var color = self.state.hoverColor;
+                    var background = hoverBackground;
+                    var color = hoverColor;
                     self.renderStuff(background, color);
                 },
                 changeHoverBack: function changeHoverBack() {
-                    var revbackground = self.state.revBackground;
-                    var revcolor = self.state.revColor;
+                    var revbackground = revBackground;
+                    var revcolor = revColor;
                     self.renderStuff(revbackground, revcolor);
                 },
                 BUTTONSTYLES: _nestingstyles2.default.create({
                     buttonStyles: {
-                        borderRadius: state.borderRadius,
-                        color: _color,
-                        fontSize: state.fontSize,
+                        borderRadius: borderRadius,
+                        color: color,
+                        fontSize: fontSize,
                         background: backgroundColor,
-                        padding: state.padding,
-                        border: state.border,
+                        padding: padding,
+                        border: border,
                         cursor: cursor,
-                        animationIterationCount: state.animationIterationCount,
-                        animationTimingFunction: state.animationTimingFunction,
-                        animationName: state.animationName,
-                        animationDuration: state.animationDuration,
-                        transform: state.transform,
-                        transformOrigin: state.transformOrigin,
-                        animationFillMode: state.animationFillMode,
-                        width: state.width,
-                        height: state.height
+                        animationIterationCount: animationIterationCount,
+                        animationTimingFunction: animationTimingFunction,
+                        animationName: animationName,
+                        animationDuration: animationDuration,
+                        transform: transform,
+                        transformOrigin: transformOrigin,
+                        animationFillMode: animationFillMode,
+                        width: width,
+                        height: height
                     },
                     '@media (max-width: 768px)': {
                         buttonStyles: {
-                            display: state.smdis
+                            display: smdis
                         }
                     },
                     '@media screen and (min-width: 769px) and (max-width: 1200px)': {
                         buttonStyles: {
-                            display: state.mddis
+                            display: mddis
                         }
                     }
                 })
             };
         case type = '2':
-            backgroundColor = arguments[4] || state.backgroundColor;
-            boxxShadow = arguments[5] || state.boxShadow;
-            var transform = arguments[6] || state.transform;
+            backgroundColor = arguments[4] || backgroundColor;
+            boxxShadow = arguments[5] || boxShadow;
+            var transform = arguments[6] || transform;
+            margin = props.margin || "5px 0 17px 0";
             cursor = "pointer";
             return {
                 changeHover: function changeHover() {
-                    var backgroundCol = state.hovBackground;
-                    var boxShadow = state.hovBoxShadow;
+                    var backgroundCol = hovBackground;
+                    var boxShadow = hovBoxShadow;
                     var transform = 'translateY(4px)';
                     self.renderStuff(backgroundCol, boxShadow, transform);
                 },
                 changeHoverBack: function changeHoverBack() {
-                    var backgroundCol = state.backgroundRev;
-                    var boxShadow = state.boxShadowRev;
-                    var transform = state.transformrev;
+                    var backgroundCol = revBackground;
+                    var boxShadow = boxShadowRev;
+                    var transform = trandformrev;
                     self.renderStuff(backgroundCol, boxShadow, transform);
                 },
                 BUTTONSTYLES: _nestingstyles2.default.create({
                     buttonStyles: {
                         display: 'inline-block',
                         padding: '15px 25px',
-                        fontSize: state.fontSize,
+                        fontSize: fontSize,
                         cursor: cursor,
                         textAlign: 'center',
                         outline: 'none',
-                        color: state.color,
+                        color: color,
                         backgroundColor: backgroundColor,
                         borderRadius: '15px',
                         boxShadow: boxxShadow,
                         transform: transform,
-                        margin: state.margin
+                        margin: margin
                     },
 
                     '@media (max-width: 768px)': {
                         buttonStyles: {
-                            display: state.smdis
+                            display: smdis
                         }
                     },
                     '@media screen and (min-width: 769px) and (max-width: 1200px)': {
                         buttonStyles: {
-                            display: state.mddis
+                            display: mddis
                         }
                     }
                 })
 
             };
         case type = '3':
-            var boxxShadow = arguments[4] || state.floatBoxShadow;
+            var boxxShadow = arguments[4] || floatBoxShadow;
             cursor = "pointer";
             return {
                 changeHover: function changeHover() {
@@ -123,51 +156,51 @@ function buttonFunction(type, props, state, self) {
                     self.renderStuff(hovBoxShadow);
                 },
                 changeHoverBack: function changeHoverBack() {
-                    var revBoxShadow = state.floatBoxShadow;
+                    var revBoxShadow = floatBoxShadow;
                     self.renderStuff(revBoxShadow);
                 },
                 BUTTONSTYLES: _nestingstyles2.default.create({
                     buttonStyles: {
-                        width: state.width,
+                        width: width,
                         display: 'block',
                         border: 'none',
-                        backgroundColor: state.backgroundColor,
+                        backgroundColor: backgroundColor,
                         padding: '14px 28px',
-                        fontSize: state.fontSize,
+                        fontSize: fontSize,
                         cursor: cursor,
                         textAlign: 'center',
-                        color: state.color,
+                        color: color,
                         boxShadow: boxxShadow,
-                        transform: state.transform,
-                        transformOrigin: state.transformOrigin,
-                        margin: state.margin
+                        transform: transform,
+                        transformOrigin: transformOrigin,
+                        margin: margin
                     },
                     '@media (max-width: 768px)': {
                         buttonStyles: {
-                            display: state.smdis
+                            display: smdis
                         }
                     },
                     '@media screen and (min-width: 769px) and (max-width: 1200px)': {
                         buttonStyles: {
-                            display: state.mddis
+                            display: mddis
                         }
                     }
                 })
 
             };
         case type = '4':
-            backgroundColor = arguments[4] || state.blockBackground;
-            _color = arguments[5] || state.frev;
+            backgroundColor = arguments[4] || blockBackground;
+            color = arguments[5] || frev;
             cursor = "pointer";
             return {
                 changeHover: function changeHover() {
-                    var backgroundCol = state.hovBackground;
-                    var col = state.revColor;
+                    var backgroundCol = hovBackground;
+                    var col = revColor;
                     self.renderStuff(backgroundCol, col);
                 },
                 changeHoverBack: function changeHoverBack() {
-                    var backgroundCol = state.colorRev;
-                    var col = state.frev;
+                    var backgroundCol = revColor;
+                    var col = frev;
                     self.renderStuff(backgroundCol, col);
                 },
                 BUTTONSTYLES: _nestingstyles2.default.create({
@@ -177,108 +210,108 @@ function buttonFunction(type, props, state, self) {
                         border: 'none',
                         backgroundColor: backgroundColor,
                         padding: '14px 28px',
-                        fontSize: state.fontSize,
+                        fontSize: fontSize,
                         cursor: cursor,
                         textAlign: 'center',
-                        color: _color,
-                        transform: state.transform,
-                        transformOrigin: state.transformOrigin
+                        color: color,
+                        transform: transform,
+                        transformOrigin: transformOrigin
                     },
                     '@media (max-width: 768px)': {
                         buttonStyles: {
-                            display: state.smdis
+                            display: smdis
                         }
                     },
                     '@media screen and (min-width: 769px) and (max-width: 1200px)': {
                         buttonStyles: {
-                            display: state.mddis
+                            display: mddis
                         }
                     }
                 })
 
             };
         case type = '5':
-            backgroundColor = arguments[4] || state.backgroundColor;
+            backgroundColor = arguments[4] || backgroundColor;
             cursor = "pointer";
             return {
                 changeHover: function changeHover() {
-                    var backgroundCol = state.hovBackground;
+                    var backgroundCol = hovBackground;
                     self.renderStuff(backgroundCol);
                 },
                 changeHoverBack: function changeHoverBack() {
-                    var backgroundCol = state.colorRev;
+                    var backgroundCol = revBackground;
                     self.renderStuff(backgroundCol);
                 },
                 BUTTONSTYLES: _nestingstyles2.default.create({
                     buttonStyles: {
-                        width: state.width,
+                        width: width,
                         display: 'block',
                         border: 'none',
                         backgroundColor: backgroundColor,
                         padding: '14px 28px',
-                        fontSize: state.fontSize,
+                        fontSize: fontSize,
                         cursor: 'pointer',
                         textAlign: 'center',
-                        color: state.color,
-                        transform: state.transform,
-                        transformOrigin: state.transformOrigin
+                        color: color,
+                        transform: transform,
+                        transformOrigin: transformOrigin
                     },
                     '@media (max-width: 768px)': {
                         buttonStyles: {
-                            display: state.smdis
+                            display: smdis
                         }
                     },
                     '@media screen and (min-width: 769px) and (max-width: 1200px)': {
                         buttonStyles: {
-                            display: state.mddis
+                            display: mddis
                         }
                     }
                 })
 
             };
         case type = 'custom':
-            var background = arguments[4] || self.state.background;
-            var _color = arguments[5] || self.state.color;
+            background = arguments[4] || background;
+            color = arguments[5] || color;
             cursor = "pointer";
             return {
                 changeHover: function changeHover() {
-                    var background = self.state.hoverBackground;
-                    var color = self.state.hoverColor;
+                    var background = hoverBackground;
+                    var color = hoverColor;
                     self.renderStuff(background, color);
                 },
                 changeHoverBack: function changeHoverBack() {
-                    var revbackground = self.state.revBackground;
-                    var revcolor = self.state.revColor;
+                    var revbackground = revBackground;
+                    var revcolor = revColor;
                     self.setState({ color: revcolor, background: revbackground });
                     self.renderStuff(revbackground, revcolor);
                 },
                 BUTTONSTYLES: _nestingstyles2.default.create({
                     buttonStyles: {
-                        borderRadius: state.borderRadius,
-                        color: _color,
-                        fontSize: state.fontSize,
+                        borderRadius: borderRadius,
+                        color: color,
+                        fontSize: fontSize,
                         background: background,
-                        padding: state.padding,
-                        border: state.border,
+                        padding: padding,
+                        border: border,
                         cursor: cursor,
-                        animationIterationCount: state.animationIterationCount,
-                        animationTimingFunction: state.animationTimingFunction,
-                        animationName: state.animationName,
-                        animationDuration: state.animationDuration,
-                        transform: state.transform,
-                        transformOrigin: state.transformOrigin,
-                        animationFillMode: state.animationFillMode,
-                        width: state.width,
-                        height: state.height
+                        animationIterationCount: animationIterationCount,
+                        animationTimingFunction: animationTimingFunction,
+                        animationName: animationName,
+                        animationDuration: animationDuration,
+                        transform: transform,
+                        transformOrigin: transformOrigin,
+                        animationFillMode: animationFillMode,
+                        width: width,
+                        height: height
                     },
                     '@media (max-width: 768px)': {
                         buttonStyles: {
-                            display: state.smdis
+                            display: smdis
                         }
                     },
                     '@media screen and (min-width: 769px) and (max-width: 1200px)': {
                         buttonStyles: {
-                            display: state.mddis
+                            display: mddis
                         }
                     }
                 })
