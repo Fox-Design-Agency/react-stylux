3f331d5d160089593da94cc11c2c1dae5da190ac
Christopher Fox <cfox1290@gmail.com>
2018-05-10 15:49:41 -0600

    rebuilt table

--- Tables/tableFunctions/tableFunctions.js
+++ Tables/tableFunctions/tableFunctions.js
@@ -17,34 +17,71 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 function tableFunction(type, props, state, self) {
     var cursor = "";
-
+    var smdis = props.smDis;
+    var mddis = props.mdDis;
+    var animationIterationCount = props.aniCount;
+    var animationTimingFunction = props.aniTime;
+    var animationName = props.aniName;
+    var animationDuration = props.aniDur;
+    var transformOrigin = props.transformOrigin;
+    var animationFillMode = props.aniFillMode;
+    var headBorderTop = props.hbordertop;
+    var headBorderRight = props.hborderRight;
+    var headBorderBottom = props.hborderBottom;
+    var headBorderLeft = props.hborderLeft;
+    var headBorder = props.hborder || '1px solid black';
+    var headVertical = props.hvertical || 'center';
+    var headWidth = props.headWidth;
+    var headHeight = props.headHeight;
+    var headMargin = props.headMargin;
+    var headPadding = props.headPadding || '5px';
+    var dBorderTop = props.dbordertop;
+    var dBorderRight = props.dborderRight;
+    var dBorderBottom = props.dborderBottom;
+    var dBorderLeft = props.dborderLeft;
+    var dBorder = props.dborder || '1px solid black';
+    var dVertical = props.dVertical || 'center';
+    var dwidth = props.dwidth;
+    var dheight = props.dheight;
+    var dmargin = props.dmargin;
+    var dpadding = props.dpadding || '5px';
+    var tableBorderTop = props.tbordertop;
+    var tableBorderRight = props.tborderRight;
+    var tableBorderBottom = props.tborderBottom;
+    var tableBorderLeft = props.tborderLeft;
+    var tableBorder = props.tborder || '1px solid black';
+    var tableMargin = props.tmargin || '5px';
+    var tablePadding = props.tpadding;
+    var tableWidth = props.tableWidth;
+    var tableHeight = props.tableHeight;
+    var tableBorderCollapse = props.borderCollaspe || 'collapse';
     switch (type) {
         case type = '1':
             cursor = "pointer";
             return {
                 TABLESTYLE: _nestingstyles2.default.create({
                     tablestyle: {
-                        borderTop: state.tableBorderTop,
-                        borderRight: state.tableBorderRight,
-                        borderBottom: state.tableBorderBottom,
-                        borderLeft: state.tableBorderLeft,
-                        border: state.tableBorder,
-                        margin: state.tableMargin,
-                        padding: state.tablePadding,
+                        borderTop: tableBorderTop,
+                        borderRight: tableBorderRight,
+                        borderBottom: tableBorderBottom,
+                        borderLeft: tableBorderLeft,
+                        border: tableBorder,
+                        margin: tableMargin,
+                        padding: tablePadding,
                         textAlign: 'center',
-                        width: state.tableWidth,
-                        height: state.tableHeight,
-                        borderCollapse: state.tableBorderCollapse,
+                        width: tableWidth,
+                        height: tableHeight,
+                        borderCollapse: tableBorderCollapse,
                         cursor: 'default'
                     },
                     '@media screen and (max-width: 768px)': {
                         tablestyle: {
-                            display: state.smDis
+                            display: smdis
                         }
                     },
                     '@media screen and (min-width: 769px) and (max-width: 1200px)': {
                         tablestyle: {
-                            display: state.mdDis
+                            display: mddis
                         }
                     }
                 }),
@@ -54,12 +91,12 @@ function tableFunction(type, props, state, self) {
                     },
                     '@media screen and (max-width: 768px)': {
                         tableres: {
-                            display: state.smDis
+                            display: smdis
                         }
                     },
                     '@media screen and (min-width: 769px) and (max-width: 1200px)': {
                         tableres: {
-                            display: state.mdDis
+                            display: mddis
                         }
                     }
                 })
