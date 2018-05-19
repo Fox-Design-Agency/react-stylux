55f457d46f318abd594be14d3250e921c00624be
Christopher Fox <cfox1290@gmail.com>
2018-05-19 10:13:33 -0600

    commented out figure for now

--- Images/Figure.js
+++ Images/Figure.js
@@ -1,106 +1,60 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-var _react = require('react');
-
-var _react2 = _interopRequireDefault(_react);
-
-require('../css/reset.css');
-
-require('../css/animations.css');
-
-require('../css/SuperStyleSheet.css');
-
-var _reactResponsive = require('react-responsive');
-
-var _reactResponsive2 = _interopRequireDefault(_reactResponsive);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-/* Start of Figure Blocks */
-var Figure = function (_React$Component) {
-    _inherits(Figure, _React$Component);
-
-    function Figure(props) {
-        _classCallCheck(this, Figure);
-
-        var _this = _possibleConstructorReturn(this, (Figure.__proto__ || Object.getPrototypeOf(Figure)).call(this, props));
-
-        _this.state = {
-            size: props.size || 'sm',
-            figid: props.figid,
-            figCapid: props.figCapid,
-            figClassName: props.figClassName,
-            figCapClassName: props.figCapClassName,
-            childs: '',
-            smdis: props.smDis || 'flex',
-            mddis: props.mdDis || 'flex',
-            margin: props.margin || '5px',
-            padding: props.padding,
-            boxShadow: props.shadow,
-            boxShadowRev: props.shadow,
-            hoverShadow: props.hovShadow
-        };
-        return _this;
-    }
-
-    _createClass(Figure, [{
-        key: 'componentDidMount',
-        value: function componentDidMount() {
-            var CHILDS = _react2.default.Children.toArray(this.props.children);
-            this.setState({ childs: CHILDS });
-        }
-    }, {
-        key: 'componentWillReceiveProps',
-        value: function componentWillReceiveProps(newProps) {
-            var CHILDS = _react2.default.Children.toArray(newProps.children);
-            return this.setState({ childs: CHILDS });
-        }
-    }, {
-        key: 'render',
-        value: function render() {
-            var caption = {
-                padding: '10px',
-                color: 'gray',
-                textAlign: 'center'
-            };
-            var smcaption = {
-                display: this.state.smDis,
-                fontSize: '6em'
-            };
-            var mdcaption = {
-                display: this.state.mdDis,
-                fontSize: '6em'
-            };
-            return _react2.default.createElement(
-                _react2.default.Fragment,
-                null,
-                _react2.default.createElement(
-                    'figure',
-                    { id: this.state.figid, className: this.state.figClassName },
-                    this.state.childs[0],
-                    _react2.default.createElement(
-                        'figcaption',
-                        { style: CAPTION.caption, id: this.state.figCapid, className: this.state.figCapClassName },
-                        this.state.childs[1]
-                    )
-                )
-            );
-        }
-    }]);
-
-    return Figure;
-}(_react2.default.Component);
-
-exports.default = Figure;
+// import React from 'react';
+// import '../css/reset.css'
+// import '../css/animations.css';
+// import '../css/SuperStyleSheet.css';
+// import MediaQuery from "react-responsive";
+// /* Start of Figure Blocks */
+// export default class Figure extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             size: props.size || 'sm',
+//             figid: props.figid,
+//             figCapid: props.figCapid,
+//             figClassName: props.figClassName,
+//             figCapClassName: props.figCapClassName,
+//             childs: '',
+//             smdis: props.smDis || 'flex',
+//             mddis: props.mdDis || 'flex',
+//             margin: props.margin || '5px',
+//             padding: props.padding,
+//             boxShadow: props.shadow,
+//             boxShadowRev: props.shadow,
+//             hoverShadow: props.hovShadow,
+//         }
+//     }
+//     componentDidMount() {
+//         const CHILDS = React.Children.toArray(this.props.children)
+//         this.setState({ childs: CHILDS })
+//     }
+//     componentWillReceiveProps(newProps) {
+//         const CHILDS = React.Children.toArray(newProps.children)
+//         return this.setState({ childs: CHILDS })
+//     }
+//     render() {
+//        let caption = {
+//             padding: '10px',
+//             color: 'gray',
+//             textAlign: 'center',
+//         }
+//         let smcaption = {
+//             display: this.state.smDis,
+//             fontSize: '6em'
+//         }
+//         let mdcaption = {
+//             display: this.state.mdDis,
+//             fontSize: '6em'
+//         }
+//         return (
+//             <React.Fragment>
+//                 <figure id={this.state.figid} className={this.state.figClassName}>
+//                     {this.state.childs[0]}
+//                     <figcaption style={caption} id={this.state.figCapid} className={this.state.figCapClassName}>
+//                         {this.state.childs[1]}
+//                     </figcaption>
+//                 </figure>
+//             </React.Fragment>
+//         )
+//     }
+// }
+"use strict";
